cc95d947add2a2a9f5d0a58cefcb59915107790c
Nikos Gianniotis <ngiann@gmail.com>
2024-03-13 12:17:32 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/GPLVM_plus_dataset.jl
@@ -0,0 +1,5 @@
+module GPLVM_plus_dataset
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GPLVM_plus_dataset
+using Test
+
+@testset "GPLVM_plus_dataset.jl" begin
+    # Write your tests here.
+end
