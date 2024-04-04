4b09465d7fd02bd37a3d79315b839709d9feb0b0
Nikos Gianniotis <ngiann@gmail.com>
2024-04-04 09:23:34 +0200

    argument for choosing value for missing entries

--- src/loadstripe82dataset.jl
+++ src/loadstripe82dataset.jl
@@ -1,8 +1,8 @@
-function loadstripe82dataset()
+function loadstripe82dataset(;replacement = missing)
 
     data = JLD2.load(joinpath(artifact"stripe82data","stripe82data.jld2"))
 
-    restframespectra = replace(reduce(hcat, data["restframespectra"]), Inf => missing)
+    restframespectra = replace(reduce(hcat, data["restframespectra"]), Inf => replacement)
 
     @printf("Returning spectra, that have been shifted to the rest frame, as an array of size %d×%d.\n", size(restframespectra,1), size(restframespectra,2))
     @printf("Also returning common grid of wavelength values, valid for all spectra, and a unique string identifier.\n")
